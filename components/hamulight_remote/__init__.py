03e206ee41108cc2d5d780229b50198dd02e186c
Foxhill67 <35631055+Foxhill67@users.noreply.github.com>
2025-03-08 22:29:27 +0100

    Update __init__.py

--- components/hamulight_remote/__init__.py
+++ components/hamulight_remote/__init__.py
@@ -1,9 +1,7 @@
 import esphome.codegen as cg
-from esphome.components import light
-from esphome.core import CORE
 
 CODEOWNERS = ["foxhill67"]
+MULTI_CONF = False
 
 hamulight_remote_ns = cg.esphome_ns.namespace('hamulight_remote')
-HamulightRemote = hamulight_remote_ns.class_('HamulightRemote', cg.Component, light.LightOutput)
-
+HamulightRemote = hamulight_remote_ns.class_('HamulightRemote', cg.Component, cg.light.LightOutput)
