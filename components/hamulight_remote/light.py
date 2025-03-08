f094e77eff199c837cc51831efc699c8d2b83a09
Foxhill67 <35631055+Foxhill67@users.noreply.github.com>
2025-03-08 21:59:59 +0100

    Update light.py

--- components/hamulight_remote/light.py
+++ components/hamulight_remote/light.py
@@ -9,9 +9,6 @@ CONFIG_SCHEMA = light.LIGHT_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(HamulightRemote),
 })
 
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
 
 
 
