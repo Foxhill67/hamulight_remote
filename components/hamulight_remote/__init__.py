87e121c17f68e1d6f58bc476cd406ab3dfe33e98
Foxhill67 <35631055+Foxhill67@users.noreply.github.com>
2025-03-08 22:32:13 +0100

    Update __init__.py

--- components/hamulight_remote/__init__.py
+++ components/hamulight_remote/__init__.py
@@ -5,4 +5,4 @@ CODEOWNERS = ["foxhill67"]
 MULTI_CONF = False
 
 hamulight_remote_ns = cg.esphome_ns.namespace('hamulight_remote')
-HamulightRemote = hamulight_remote_ns.class_('HamulightRemote', cg.Component, cg.light.LightOutput)
+HamulightRemote = hamulight_remote_ns.class_('HamulightRemote', cg.Component, light.LightOutput) # Use light.LightOutput
