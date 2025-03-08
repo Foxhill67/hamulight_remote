c8e9618b08433b9d31a3f3b0d3545375ee50b5a9
Foxhill67 <35631055+Foxhill67@users.noreply.github.com>
2025-03-08 19:23:46 +0100

    Update light.py

--- components/hamulight_remote/light.py
+++ components/hamulight_remote/light.py
@@ -1,8 +1,9 @@
-from . import hamulight_remote_ns, HamulightRemote
-from esphome.components import light
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components import light
 from esphome.const import CONF_ID
+
+from . import hamulight_remote_ns, HamulightRemote
 
 CONFIG_SCHEMA = light.LIGHT_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(HamulightRemote),
@@ -12,3 +13,5 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await light.register_light(var, config)
+
+
