7f233fbbcdedc656b42621ab1d2d6677601c881b
Foxhill67 <35631055+Foxhill67@users.noreply.github.com>
2025-03-08 21:54:57 +0100

    Update __init__.py

--- components/hamulight_remote/__init__.py
+++ components/hamulight_remote/__init__.py
@@ -7,3 +7,7 @@ CODEOWNERS = ["foxhill67"]
 hamulight_remote_ns = cg.esphome_ns.namespace('hamulight_remote')
 HamulightRemote = hamulight_remote_ns.class_('HamulightRemote', cg.Component, light.LightOutput)
 
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)  # Registers the component
+    yield light.register_light(var, config)  # Registers the light
