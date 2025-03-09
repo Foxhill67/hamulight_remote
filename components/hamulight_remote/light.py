d6adec620bf0c17ac50053724cbfeb44fc0706d3
Foxhill67 <35631055+Foxhill67@users.noreply.github.com>
2025-03-09 09:51:49 +0100

    Update light.py

--- components/hamulight_remote/light.py
+++ components/hamulight_remote/light.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import light
-from esphome.const import CONF_ID
+from esphome.const import CONF_OUTPUT_ID
 import logging
 
 from . import HamulightRemote
@@ -12,7 +12,7 @@ CONFIG_SCHEMA = light.LIGHT_SCHEMA.extend({
 
 async def to_code(config):
     logging.info("hamulight_remote: light.py to_code() called")
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
     logging.info("hamulight_remote: var called")
     id = config[CONF_ID].id
     logging.info("hamulight_remote: id called")
