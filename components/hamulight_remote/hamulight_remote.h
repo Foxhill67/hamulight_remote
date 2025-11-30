91e3300550412d50535661a963c14b857b1ad8e0
Foxhill67 <35631055+Foxhill67@users.noreply.github.com>
2025-11-30 22:00:08 +0100

    Fix switch off lights

--- components/hamulight_remote/hamulight_remote.h
+++ components/hamulight_remote/hamulight_remote.h
@@ -52,7 +52,7 @@ class HamulightRemote : public Component, public light::LightOutput {
         percentage = 1;
         ESP_LOGD("main", "Turn light on");
       } else {
-        slowDim(newPercentage, 1);
+        //slowDim(newPercentage, 1);
         sendSignal(signalToggle, 10, 0);
         percentage = 0;
         ESP_LOGD("main", "Turn light off");
